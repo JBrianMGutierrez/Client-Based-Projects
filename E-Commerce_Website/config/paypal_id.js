f8e01ff769eb7416051a886239d220a94993bd69
John Brian M. Gutierrez <jinxmugen@gmail.com>
2018-09-01 10:59:32 +0800

    Scret
    Scret

--- E-Commerce_Website/config/paypal_id.js
+++ E-Commerce_Website/config/paypal_id.js
@@ -2,6 +2,6 @@ var paypal = require('paypal-rest-sdk');
 
 paypal.configure({
     'mode': 'sandbox', //sandbox or live
-    'client_id': 'ATcbdsvvqYqf8lyCxToO_7M2wV4JiVGg9eOJGh2mdA5qV90qv0xLe_KJWTtF49n4d5FaWScXE0TLoVrB',
-    'client_secret': 'EFNWACnnou9qfkKZWFWf487dSHpaN8Kj6HIKi9Tg1wiE4XDvy7nruVNJoeDaO3Xtx4QLZmOcq2_85GaS'
+    'client_id': 'Scret',
+    'client_secret': 'Scret'
 });
